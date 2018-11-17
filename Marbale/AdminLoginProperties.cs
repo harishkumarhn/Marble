2a525a6f8e8f784c423d0b8b1285bd997449b729
Shridhar <shridhargnaik31@gmail.com>
2018-11-17 05:21:15 -0800

    Month one with login screen

---
+++ Marbale/AdminLoginProperties.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marbale
+{
+  public  class AdminLoginProperties
+    {
+        public string UserId { get; set; }
+        public string Password { get; set; }
+        public bool Status { get; set; }
+        public int FailedAttempt { get; set; }
+        public bool LoginStatus { get; set; }
+    }
+}
