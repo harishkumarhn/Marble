13cd5f124d1f8ee494e1dc3f33cff71403257992
harishkumarhn <harishhn.cs@gmail.com>
2018-12-31 00:09:54 +0530

    ProductDetail

---
+++ Marbale.Admin/Products/ProductDetail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Marbale.Products
+{
+    public partial class ProductDetail : Form
+    {
+        public ProductDetail()
+        {
+            InitializeComponent();
+        }
+
+        private void chk_agCard_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
