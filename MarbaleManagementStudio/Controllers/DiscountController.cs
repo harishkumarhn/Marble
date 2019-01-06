ea2797b0711e2c6ed1f83e182486a2ae9e0def28
harishkumarhn <harishhn.cs@gmail.com>
2019-01-06 11:19:43 +0530

    Added mvc project

---
+++ MarbaleManagementStudio/Controllers/DiscountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarbaleManagementStudio.Controllers
+{
+    public class DiscountController : Controller
+    {
+        //
+        // GET: /Discount/
+
+        public ActionResult Index()
+        {
+            return View("Discount");
+        }
+
+    }
+}
