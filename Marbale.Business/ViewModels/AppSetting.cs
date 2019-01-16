3a5dc682f952b9927ba2732ce76542b4b741f0b4
harishkumarhn <harishhn.cs@gmail.com>
2019-01-17 00:35:26 +0530

    sp added

---
+++ Marbale.Business/ViewModels/AppSetting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marble.Business.ViewModels
+{
+    public class AppSetting
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string ScreenGroup { get; set; }
+    }
+}
