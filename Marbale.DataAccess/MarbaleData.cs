aebb329e3d40190f649aaf547ec26fc572069618
harishkumarhn <harishhn.cs@gmail.com>
2019-01-16 22:54:35 +0530

    updated sps

--- Marbale.DataAccess/MarbaleData.cs
+++ Marbale.DataAccess/MarbaleData.cs
@@ -37,30 +37,7 @@ namespace Marbale.DataAccess
             sqlParameters[8] = new SqlParameter("@poslevel", posLevel);
             sqlParameters[9] = new SqlParameter("@updatedby", updatedby);
 
-            return conn.executeUpdate("sp_UpdateAppSettings", sqlParameters);
-        }
-
-        public DataTable searchByName(string _username)
-        {
-            string query = string.Format("select * from appsettings");
-            SqlParameter[] sqlParameters = new SqlParameter[2];
-            sqlParameters[0] = new SqlParameter("@t01_firstname", SqlDbType.VarChar);
-            sqlParameters[0].Value = Convert.ToString(_username);
-            sqlParameters[1] = new SqlParameter("@t01_lastname", SqlDbType.VarChar);
-            sqlParameters[1].Value = Convert.ToString(_username);
-            return conn.executeSelectQuery(query, sqlParameters);
-        }
-
-        /// <method>
-        /// Get User Email By Id and return DalaTable
-        /// </method>
-        public DataTable searchById(string _id)
-        {
-            string query = "select * from [t01_id] where t01_id = @t01_id";
-            SqlParameter[] sqlParameters = new SqlParameter[1];
-            sqlParameters[0] = new SqlParameter("@t01_id", SqlDbType.VarChar);
-            sqlParameters[0].Value = Convert.ToString(_id);
-            return conn.executeSelectQuery(query, sqlParameters);
+            return conn.executeUpdate("sp_UpdateSettings", sqlParameters);
         }
 
     }
