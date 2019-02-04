f968f4e57fc80e37715f13538d8b5d6e5be9dd6d
Shridhar <shridhargnaik31@gmail.com>
2019-02-03 19:29:20 -0800

    Config vhanges

---
+++ Marbale.ManagementStudio/Controllers/SiteSetupController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarbaleManagementStudio.Controllers
+{
+    public class SiteSetupController : Controller
+    {
+        //
+        // GET: /SiteSetup/
+
+        public ActionResult Configuration()
+        {
+            return View();
+        }
+        public ActionResult Settings()
+        {
+            return View();
+        }
+        public ActionResult Values()
+        {
+            return PartialView();
+        }
+        public ActionResult POS()
+        {
+            return PartialView();
+        }
+        public ActionResult Card()
+        {
+            return PartialView();
+        }
+        
+
+    }
+}
