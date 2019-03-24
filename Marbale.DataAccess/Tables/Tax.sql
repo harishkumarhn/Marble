94063c751431d28c84fcb8475ba2a8df2150d042
Shridhar <shridhargnaik31@gmail.com>
2019-03-24 05:33:23 -0700

    Tables

---
+++ Marbale.DataAccess/Tables/Tax.sql
@@ -0,0 +1,19 @@
+USE [Marbale]
+GO
+
+/****** Object:  Table [dbo].[Tax]    Script Date: 3/24/2019 5:31:35 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tax](
+	[TaxId] [int] IDENTITY(1,1) NOT NULL,
+	[TaxName] [varchar](100) NULL,
+	[TaxPercent] [decimal](18, 0) NULL,
+	[ActiveFlag] [bit] NULL
+) ON [PRIMARY]
+GO
+
+
