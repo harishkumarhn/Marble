c881260d8bdfcdbe1a7ff4ca9a0ea616e53a8020
harishkumarhn <harishhn.cs@gmail.com>
2019-06-17 23:57:53 +0530

    reports project with customer and card table

---
+++ Marbale.Reports/Controllers/ReportController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Marbale.Reports.Controllers
+{
+    public class ReportController : Controller
+    {
+        //
+        // GET: /Report/
+
+        public ActionResult Run()
+        {
+            return View();
+        }
+        public ActionResult Schedule()
+        {
+            return View();
+        }
+        public ActionResult Management()
+        {
+            return View();
+        }
+
+    }
+}
