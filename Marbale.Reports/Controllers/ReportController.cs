0cff9608923b411bc741f34b763bb1216778b9d5
harishkumarhn <harishhn.cs@gmail.com>
2019-07-07 01:31:42 +0530

    report

--- Marbale.Reports/Controllers/ReportController.cs
+++ Marbale.Reports/Controllers/ReportController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Marbale.Reports.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,18 @@ namespace Marbale.Reports.Controllers
 
         public ActionResult Run()
         {
+            return View();
+        }
+        public ActionResult AllReports()
+        {
+            List<ReportMenuViewModel> modelList = new List<ReportMenuViewModel>();
+            modelList.Add(new ReportMenuViewModel()
+            {
+                Name = "Transaction",
+                Column1 = new Column1() { DisplayName = "Transaction Report1", URL = "\\transaction\\transactionReport" },
+                Column2 = new Column2() { DisplayName = "Transaction Report2", URL = "\\transaction\\transactionReport" }
+            });
+            ViewBag.models = modelList;
             return View();
         }
         public ActionResult Schedule()
