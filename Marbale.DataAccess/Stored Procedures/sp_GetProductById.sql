8380e9036f082d3147b95bcef81772f456463b7b
Shridhar <shridhargnaik31@gmail.com>
2019-08-15 02:33:48 -0700

    Issues

--- Marbale.DataAccess/Stored Procedures/sp_GetProductById.sql
+++ Marbale.DataAccess/Stored Procedures/sp_GetProductById.sql
@@ -1,29 +1,32 @@
 USE [Marbale]
 GO
-
-/****** Object:  StoredProcedure [dbo].[sp_GetProduct]    Script Date: 1/15/2019 10:11:29 PM ******/
+/****** Object:  StoredProcedure [dbo].[sp_GetProductById]    Script Date: 8/15/2019 1:19:50 AM ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
--- =============================================
--- Author:		Harish
--- Create date: <Create Date,,>
--- Description:	<Description,,>
--- =============================================
-CREATE PROCEDURE [dbo].[sp_GetProduct] 
-	@id int
-AS
-BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
-
-    -- Insert statements for procedure here
-	SELECT * from Product where Id = @id
-END
-GO
-
-
+CREATE PROCEDURE [dbo].[sp_GetProductById]     
+ @id int    
+AS    
+BEGIN    
+ -- SET NOCOUNT ON added to prevent extra result sets from    
+ -- interfering with SELECT statements.    
+ SET NOCOUNT ON;    
+  declare @expiredate datetime  
+  declare @withoutExpireDate datetime 
+  declare @cardvalidFor int
+  declare @startDate datetime
+  select @cardvalidFor= DefaultValue  from Settings where name ='CARD_VALIDITY'
+   select @expiredate = ExpiryDate from Product where id=@id
+   select @startDate = startDate from Product where id=@id
+  select @withoutExpireDate =DATEADD(DAY,@cardvalidFor,@startDate)  
+ SELECT     
+ Courtesy,  
+ Id ,Name ,Type ,POSCounter, Active, DisplayInPOS ,DisplayGroup ,Category ,
+ AutoGenerateCardNumber ,OnlyVIP, Price, FaceValue, TaxInclusive, TaxPercentage, FinalPrice, EffectivePrice,    
+  LastUpdatedBy, LastUpdatedDate, Bonus, LastUpdatedUser ,TaxName, StartDate as 'StartDate', Games ,    
+  CreditsPlus, Credits ,CardValidFor,case when isnull(@expiredate,'') ='' then @withoutExpireDate else ExpiryDate end as 'ExpiryDate'
+  --ExpiryDate 'ExpiryDate'    
+    
+  from Product where Id = @id    
+END 
--- Marbale.DataAccess/Stored Procedures/sp_InsertOrUpdateProduct.sql
+++ Marbale.DataAccess/Stored Procedures/sp_InsertOrUpdateProduct.sql
@@ -1,90 +1,85 @@
-USE [Marbale]
-GO
-/****** Object:  StoredProcedure [dbo].[sp_InsertOrUpdateProduct]    Script Date: 8/11/2019 4:43:13 AM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+        
+-- =============================================        
+-- Author:  Harish   Modified:Shridhar     
+-- =============================================        
+        
+CREATE PROCEDURE [dbo].[sp_InsertOrUpdateProduct]        
+@name varchar(150),        
+@type varchar(50) = null,        
+@active bit=null,        
+@price int=null,        
+@effectivePrice int=null,        
+@finalPrice int=null,      
+@faceValue int=null,        
+@displayGroup varchar(50)=null,        
+@displayInPOS bit=null,        
+@autoGenerateCardNumber bit=null,        
+@category varchar(50)=null,        
+@onlyVIP bit=null,        
+@posCounter varchar(50)=null,        
+@taxInclusive bit=null,        
+@taxPercentage int=null,        
+@id int=null  ,      
+@Bonus int =null,      
+@LastUpdatedUser varchar(max)=null,      
+@TaxName varchar(max)=null,      
+@StartDate datetime=null,      
+@LastUpdatedDate datetime=null,      
+@Games int=null,      
+@CreditsPlus int=null,      
+@Credits int=null,      
+@CardValidFor int=null  ,    
+@Courtesy bigint=null   ,
+@ExpiryDate datetime =null   
+AS        
+BEGIN        
+ -- SET NOCOUNT ON added to prevent extra result sets from        
+ -- interfering with SELECT statements.        
+ SET NOCOUNT ON;        
+ declare @TotalDays int declare @expire datetime      
+  select @TotalDays= Defaultvalue from Settings where name ='CARD_VALIDITY'      
+  select @expire=   DATEADD(day, @TotalDays, getdate())       
       
--- =============================================      
--- Author:  Harish      
--- =============================================      
+if not exists (select id from Product with (updlock,serializable) where Id = @id)        
+begin        
+insert into Product(        
+[Name],        
+[Type],        
+Active,        
+Price,        
+EffectivePrice,        
+FaceValue,        
+DisplayGroup,        
+DisplayInPOS,        
+AutoGenerateCardNumber,        
+Category,        
+OnlyVIP,        
+POSCounter,        
+TaxInclusive,        
+TaxPercentage,      
+Bonus,      
+LastUpdatedUser,TaxName,LastUpdatedDate,Games,CreditsPlus,Credits,CardValidFor,Courtesy,  
+finalprice    ,
+ExpiryDate ,StartDate
       
-CREATE PROCEDURE [dbo].[sp_InsertOrUpdateProduct]      
-@name varchar(150),      
-@type varchar(50) = null,      
-@active bit=null,      
-@price int=null,      
-@effectivePrice int=null,      
-@finalPrice int=null,    
-@faceValue int=null,      
-@displayGroup varchar(50)=null,      
-@displayInPOS bit=null,      
-@autoGenerateCardNumber bit=null,      
-@category varchar(50)=null,      
-@onlyVIP bit=null,      
-@posCounter varchar(50)=null,      
-@taxInclusive bit=null,      
-@taxPercentage int=null,      
-@id int=null  ,    
-@Bonus int =null,    
-@LastUpdatedUser varchar(max)=null,    
-@TaxName varchar(max)=null,    
---@StartDate datetime=null,    
-@LastUpdatedDate datetime=null,    
-@Games int=null,    
-@CreditsPlus int=null,    
-@Credits int=null,    
-@CardValidFor int=null  ,  
-@Courtesy bigint=null    
-AS      
-BEGIN      
- -- SET NOCOUNT ON added to prevent extra result sets from      
- -- interfering with SELECT statements.      
- SET NOCOUNT ON;      
- declare @TotalDays int declare @expire datetime    
-  select @TotalDays= Defaultvalue from Settings where name ='CARD_VALIDITY'    
-  select @expire=   DATEADD(day, @TotalDays, getdate())     
-    
-if not exists (select id from Product with (updlock,serializable) where Id = @id)      
-begin      
-insert into Product(      
-[Name],      
-[Type],      
-Active,      
-Price,      
-EffectivePrice,      
-FaceValue,      
-DisplayGroup,      
-DisplayInPOS,      
-AutoGenerateCardNumber,      
-Category,      
-OnlyVIP,      
-POSCounter,      
-TaxInclusive,      
-TaxPercentage,    
-Bonus,    
-LastUpdatedUser,TaxName,LastUpdatedDate,Games,CreditsPlus,Credits,CardValidFor,Courtesy,
-finalprice   
-    
-    
-)       
-Values(@name,@type,@active,@price,@effectivePrice,@faceValue,@displayGroup,@displayInPOS,      
-@autoGenerateCardNumber,@category,@onlyVIP,@posCounter,@taxInclusive,@taxPercentage,@Bonus,@LastUpdatedUser,@TaxName,    
-@LastUpdatedDate,@Games,@CreditsPlus,@Credits,@CardValidFor,@Courtesy,@finalPrice)      
-end      
-else      
-begin      
- update Product set [Name]= @name,[Type]=@type,[Active]=@active,Price=@price,FinalPrice=@finalPrice,      
-     EffectivePrice=@effectivePrice,FaceValue=@faceValue,DisplayGroup=@displayGroup,      
-     DisplayInPOS=@displayInPOS,AutoGenerateCardNumber=@autoGenerateCardNumber,Category=@category,      
-     OnlyVIP=@onlyVIP,POSCounter=@posCounter,TaxInclusive=@taxInclusive,TaxPercentage=@taxPercentage  ,    
-  Bonus=@Bonus,LastUpdatedUser=@LastUpdatedUser,TaxName=@TaxName,Games=@Games,CreditsPlus=@CreditsPlus,    
-  Credits=@Credits,CardValidFor=@CardValidFor , Courtesy=@Courtesy  
-    
-    
-    
-     where Id=@id      
-end      
       
-END      
+)         
+Values(@name,@type,@active,@price,@effectivePrice,@faceValue,@displayGroup,@displayInPOS,        
+@autoGenerateCardNumber,@category,@onlyVIP,@posCounter,@taxInclusive,@taxPercentage,@Bonus,@LastUpdatedUser,@TaxName,      
+@LastUpdatedDate,@Games,@CreditsPlus,@Credits,@CardValidFor,@Courtesy,@finalPrice,@ExpiryDate,@StartDate)        
+end        
+else        
+begin        
+ update Product set [Name]= @name,[Type]=@type,[Active]=@active,Price=@price,FinalPrice=@finalPrice,        
+     EffectivePrice=@effectivePrice,FaceValue=@faceValue,DisplayGroup=@displayGroup,        
+     DisplayInPOS=@displayInPOS,AutoGenerateCardNumber=@autoGenerateCardNumber,Category=@category,        
+     OnlyVIP=@onlyVIP,POSCounter=@posCounter,TaxInclusive=@taxInclusive,TaxPercentage=@taxPercentage  ,      
+  Bonus=@Bonus,LastUpdatedUser=@LastUpdatedUser,TaxName=@TaxName,Games=@Games,CreditsPlus=@CreditsPlus,      
+  Credits=@Credits,CardValidFor=@CardValidFor , Courtesy=@Courtesy    ,ExpiryDate=@ExpiryDate
+      
+      
+      
+     where Id=@id        
+end        
+        
+END 
