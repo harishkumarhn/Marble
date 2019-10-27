0059d91b016c158bb9de157e25ea4908897d9be7
harishkumarhn <harishhn.cs@gmail.com>
2019-10-27 23:07:21 +0530

    inventory

--- Marbale.ManagementStudio/Controllers/CardsController.cs
+++ Marbale.ManagementStudio/Controllers/CardsController.cs
@@ -32,70 +32,70 @@ namespace MarbaleManagementStudio.Controllers
             return View();
         }
 
-         [HttpGet]
-         public ActionResult ViewCards(CardsModel c)
-         {
-             var data1 = cardBussiness.gettechcardtype();
-             Session["TechCardType"] = data1;
-             List<CardsModel> data = cardBussiness.GetAllCards(c);
-             ViewBag.cardsDetails = data;
-             return View();
-         }
+        [HttpGet]
+        public ActionResult ViewCards(CardsModel c)
+        {
+            var data1 = cardBussiness.gettechcardtype();
+            Session["TechCardType"] = data1;
+            List<CardsModel> data = cardBussiness.GetAllCards(c);
+            ViewBag.cardsDetails = data;
+            return View();
+        }
 
-    public ActionResult Cards(int CardId)
-    {
-        var data = cardBussiness.gettechcardtype();
-        Session["TechCardType"] = data;
-        CardsModel card = cardBussiness.getCardById(CardId);
-        return View(card);
-    }
-    public ActionResult InsertOrUpdateCards(CardsModel product)
-         {
-             try
-             {
-                 int result= cardBussiness.InsertOrUpdateCards(product);
-             }
-             catch (Exception e)
-             {
-                 throw e;
-             }
-             return RedirectToAction("ViewCards", "Cards");
-         }
+        public ActionResult Cards(int CardId)
+        {
+            var data = cardBussiness.gettechcardtype();
+            Session["TechCardType"] = data;
+            CardsModel card = cardBussiness.getCardById(CardId);
+            return View(card);
+        }
+        public ActionResult InsertOrUpdateCards(CardsModel product)
+        {
+            try
+            {
+                int result = cardBussiness.InsertOrUpdateCards(product);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return RedirectToAction("ViewCards", "Cards");
+        }
         #endregion
-    #region DeleteCard
-    public JsonResult DeleteCard(int Id)
-    {
-        int i = cardBussiness.DeleteCardById(Id);
-        return Json(i,JsonRequestBehavior.AllowGet);
-    }
+        #region DeleteCard
+        public JsonResult DeleteCard(int Id)
+        {
+            int i = cardBussiness.DeleteCardById(Id);
+            return Json(i, JsonRequestBehavior.AllowGet);
+        }
 
-#endregion
-    #region Inventory
+        #endregion
+        #region Inventory
 
-    public ActionResult Inventory()
-         {
-           Inventory m = new Marbale.BusinessObject.Cards.Inventory();
-              List<Inventory>data = cardBussiness.GetInventory(m);
-              if (data.Count > 0)
-              {
-                  ViewBag.TotalNumberOfCards = data[0].TotalNumberOfCards;
-              }
-              else
-              {
-                  ViewBag.TotalNumberOfCards = 0;
-              }
-              return View();
-         }
-         public int AddDeleteInventory(Inventory inventory)
-         {
-         int NoOfCards=    cardBussiness.AddDeleteInventory(inventory);
-         return NoOfCards;
-         }
-         public ActionResult GetInventory(Inventory myObject)
-         {
-             List<Inventory> inventory = cardBussiness.GetInventory(myObject);
-             return Json(inventory, JsonRequestBehavior.AllowGet);
-         }
+        public ActionResult Inventory()
+        {
+            Inventory m = new Marbale.BusinessObject.Cards.Inventory();
+            List<Inventory> data = cardBussiness.GetInventory(m);
+            if (data.Count > 0)
+            {
+                ViewBag.TotalNumberOfCards = data[0].TotalNumberOfCards;
+            }
+            else
+            {
+                ViewBag.TotalNumberOfCards = 0;
+            }
+            return View();
+        }
+        public int AddDeleteInventory(Inventory inventory)
+        {
+            int NoOfCards = cardBussiness.AddDeleteInventory(inventory);
+            return NoOfCards;
+        }
+        public ActionResult GetInventory(Inventory myObject)
+        {
+            List<Inventory> inventory = cardBussiness.GetInventory(myObject);
+            return Json(inventory, JsonRequestBehavior.AllowGet);
+        }
 
         #endregion
     }
