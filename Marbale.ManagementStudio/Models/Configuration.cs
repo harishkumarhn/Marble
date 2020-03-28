e94fa574f1ef81854fc98af2a49152a941844833
hhn <hhn@teksystems.com>
2020-03-28 09:21:11 -0400

    Revert "added fields in Configuration.cshtml and changed alignment"
    This reverts commit e29bc098f206c9c193e0a6209c8c9d9fd86d4bc4.
    
    # Conflicts:
    #	.vs/Marbale/v14/.suo
    #	.vs/config/applicationhost.config
    #	Marbale.BusinessObject/Marbale.BusinessObject.csproj
    #	Marbale.DataAccess/DBConnection.cs
    #	Marbale.ManagementStudio/Views/Game/Configuration.cshtml

--- Marbale.ManagementStudio/Models/Configuration.cs
+++ Marbale.ManagementStudio/Models/Configuration.cs
@@ -18,34 +18,20 @@ namespace MarbaleManagementStudio.Models
         public int TicketPulseGap { get; set; }
         [DisplayName("Reverse Display Direction")]
         public bool ReverseDisplayDirection { get; set; }
-        [DisplayName("Default Theme")]
-        public int DefaultTheme { get; set; }
-        [DisplayName("Start Screen Number")]
-        public int StartScreenNumber { get; set; }
         [DisplayName("Balance Delay")]
         public int BalanceDelay { get; set; }
-        [DisplayName("Enable Reset Pulse")]
-        public bool EnableResetPulse { get; set; }
         [DisplayName("Min Seconds Between Repeat Play")]
         public bool MinSecondsBetweenRepeatPlay { get; set; }
-        [DisplayName("Enable Ext Antenna")]
-        public bool EnableExtAntenna { get; set; }
         [DisplayName("Out Of Service Theme")]
         public int OutOfServiceTheme { get; set; }
-        [DisplayName("Free Play Theme")]
-        public int FreePlayTheme { get; set; }
         [DisplayName("Coin Pulse Width")]
         public int CoinPulseWidth { get; set; }
         [DisplayName("Coin Pulse Gap")]
         public int CoinPulseGap { get; set; }
         [DisplayName("Sensor Interval")]
         public int SensorInterval { get; set; }
-        [DisplayName("Disable Tickets")]
+        [DisplayName("Sensor Interval")]
         public bool DisableTickets { get; set; }
-        [DisplayName("Coin Pusher Machine")]
-        public bool CoinPusherMachine { get; set; }
-        [DisplayName("Debug Mode")]
-        public int DebugMode { get; set; }
         public int CardRetries { get; set; }
         public int DisplayLanguage { get; set; }
         public int MaxTicketPerGamePlay { get; set; }
