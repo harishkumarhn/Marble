7eb4115bc0b0b2a7d89b9242fb7d539ea1fd1eda
hhn <hhn@teksystems.com>
2020-04-26 09:31:02 -0400

    MVC upgrage

--- Marbale.ManagementStudio/Global.asax.cs
+++ Marbale.ManagementStudio/Global.asax.cs
@@ -1,43 +1,35 @@
-﻿using MarbaleManagementStudio.Controllers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Timers;
 using System.Web;
-using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-namespace MarbaleManagementStudio
+namespace Marbale.ManagementStudio
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            // Code that runs on application startup
-            System.Timers.Timer myTimer = new System.Timers.Timer();
-            // Set the Interval to 5 seconds (5000 milliseconds).
-            myTimer.Interval = 47000;
-            myTimer.AutoReset = true;
-            myTimer.Elapsed += new ElapsedEventHandler(myTimer_Elapsed);
-            myTimer.Enabled = true;
+            //// Code that runs on application startup
+            //System.Timers.Timer myTimer = new System.Timers.Timer();
+            //// Set the Interval to 5 seconds (5000 milliseconds).
+            //myTimer.Interval = 47000;
+            //myTimer.AutoReset = true;
+            //myTimer.Elapsed += new ElapsedEventHandler(myTimer_Elapsed);
+            //myTimer.Enabled = true;
         }
-        public void myTimer_Elapsed(object source, System.Timers.ElapsedEventArgs e)
-        {
-            SiteSetupController s = new SiteSetupController();
-            HttpPostedFileBase fileUploader=null;
-            HttpPostedFileBase fileUploader1=null;
-          //  s.email_send( fileUploader,fileUploader1);
-        }
+        //public void myTimer_Elapsed(object source, System.Timers.ElapsedEventArgs e)
+        //{
+        //    SiteSetupController s = new SiteSetupController();
+        //    HttpPostedFileBase fileUploader = null;
+        //    HttpPostedFileBase fileUploader1 = null;
+        //    //  s.email_send( fileUploader,fileUploader1);
+        //}
     }
 }
