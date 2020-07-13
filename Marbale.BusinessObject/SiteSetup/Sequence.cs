cd45c62333171ed1ec9cd01585cacd9429622bcc
hhn <hhn@teksystems.com>
2020-07-13 19:37:45 +0530

    sequence

---
+++ Marbale.BusinessObject/SiteSetup/Sequence.cs
@@ -0,0 +1,14 @@
+﻿namespace Marbale.BusinessObject.SiteSetup
+{
+    public class Sequence
+    {
+        public int SequenceId { get; set; }
+        public string SequenceName { get; set; }
+        public int Seed { get; set; }
+        public int Increment { get; set; }
+        public int CurrentValue { get; set; }
+        public string Prefix { get; set; }
+        public string Suffix { get; set; }
+
+    }
+}
